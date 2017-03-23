99ac3b545e264ea63f7e3e6692976c1582447834
Justin Jackson <jackson.jt@att.net>
2017-03-23 14:56:39 -0700

    Update mkall.sh

--- LAB6PRE/mkall.sh
+++ LAB6PRE/mkall.sh
@@ -1,9 +1,24 @@
 #!/bin/sh
-gcc -m32 -o super super.c;
-gcc -m32 -o inode inode.c;
-gcc -m32 -o imap imap.c;
-gcc -m32 -o ialloc ialloc.c;
-gcc -m32 -o gd gd.c;
-gcc -m32 -o dir dir.c;
-gcc -m32 -o bmap.c;
-gcc -m32 -o balloc.c;
+echo "gcc -m32 -o super super.c"
+gcc -m32 -o super super.c
+
+echo "gcc -m32 -o inode inode.c"
+gcc -m32 -o inode inode.c
+
+echo "gcc -m32 -o imap imap.c"
+gcc -m32 -o imap imap.c
+
+echo "gcc -m32 -o ialloc ialloc.c"
+gcc -m32 -o ialloc ialloc.c
+
+echo "gcc -m32 -o gd gd.c"
+gcc -m32 -o gd gd.c
+
+echo "gcc -m32 -o dir dir.c"
+gcc -m32 -o dir dir.c
+
+echo "gcc -m32 -o bmap bmap.c"
+gcc -m32 -o bmap bmap.c
+
+echo "gcc -m32 -o balloc balloc.c"
+gcc -m32 -o balloc balloc.c
