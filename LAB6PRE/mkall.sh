db2ba87518e7cac623c89c8c8a832576d0e59019
Justin Jackson <jackson.jt@att.net>
2017-03-23 14:50:58 -0700

    Update mkall.sh

--- LAB6PRE/mkall.sh
+++ LAB6PRE/mkall.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-gcc -m32 -o super super.c
-gcc -m32 -o inode inode.c
-gcc -m32 -o imap imap.c
-gcc -m32 -o ialloc ialloc.c
-gcc -m32 -o gd gd.c
-gcc -m32 -o dir dir.c
-gcc -m32 -o bmap.c
-gcc -m32 -o balloc.c
+gcc -m32 -o super super.c;
+gcc -m32 -o inode inode.c;
+gcc -m32 -o imap imap.c;
+gcc -m32 -o ialloc ialloc.c;
+gcc -m32 -o gd gd.c;
+gcc -m32 -o dir dir.c;
+gcc -m32 -o bmap.c;
+gcc -m32 -o balloc.c;
