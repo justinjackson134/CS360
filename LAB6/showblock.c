05393e0b8dc60ae2121cca65555400af1de2e59d
Justin Jackson <jackson.jt@att.net>
2017-03-29 21:41:50 -0700

    trying to set ip ptr to the new inode num

--- LAB6/showblock.c
+++ LAB6/showblock.c
@@ -214,7 +214,7 @@ showblock() {
     } 
     //-------------------------------------------------------
     //use inumber to read in its INODE and let ip --> this INODE 
-    //ip = inumber;
+    ip = (SUPER *) inumber;
   }
     
   // if you reach here, you must have ip --> the INODE of pathname.
