e3993f65aea8178e3dfc014862d2f6a468a37f97
jmsadoian <jacob.sadoian@wsu.edu>
2017-03-30 13:54:51 -0700

    added data block for loop

--- LAB6/showblock.c
+++ LAB6/showblock.c
@@ -140,31 +140,34 @@ get_tokens_from_pathname() {
 char dbuf[1024];
 // Searches through data blocks to find entry specified by pathname
 int search(INODE * inodePtr, char * name) {
-  printf("\nSEARCHING FOR: %s", name);
+	printf("\nSEARCHING FOR: %s", name);
+	for (int i = 0; i < 12; i++) {
+		if (inodePtr->i_block[i] == 0)
+			return 0;
+		get_block(fd, inodePtr->i_block[i], dbuf);  // char dbuf[1024]
 
-  get_block(fd, inodePtr->i_block[0], dbuf);  // char dbuf[1024]
+		DIR *dp = (SUPER *)dbuf;
+		char *cp = dbuf;
 
-  DIR *dp = (SUPER *)dbuf;
-  char *cp = dbuf;
+		while (cp < &dbuf[1024])
+		{
+			//use dp-> to print the DIR entries as  [inode rec_len name_len name]
+			//printf("\n - DIR ENTRY - rec_len: %d, name_len: %d, name: %s", dp->rec_len, dp->name_len, dp->name);
+			if (strcmp(name, dp->name) == 0)
+			{
+				//printf("\n - Name: %s == %s", name, dp->name);
+				printf("\n - Found at INODE: %d\n", dp->inode);
+				return dp->inode;
+			}
+			//printf("\n - Name: %s != %s", name, dp->name);
+			cp += dp->rec_len;
+			dp = (DIR *)cp;
 
-  while (cp < &dbuf[1024])
-  {
-    //use dp-> to print the DIR entries as  [inode rec_len name_len name]
-    //printf("\n - DIR ENTRY - rec_len: %d, name_len: %d, name: %s", dp->rec_len, dp->name_len, dp->name);
-    if(strcmp(name, dp->name) == 0)
-    {
-      //printf("\n - Name: %s == %s", name, dp->name);
-      printf("\n - Found at INODE: %d\n", dp->inode);
-      return dp->inode;
-    }
-      //printf("\n - Name: %s != %s", name, dp->name);
-      cp += dp->rec_len;
-      dp = (DIR *) cp;
-
-      //getchar();
-  }
-  printf(" - Not Found\n");
-  return 0;
+			//getchar();
+		}
+		printf(" - Not Found\n");
+		return 0;
+	}
 }
 
 
