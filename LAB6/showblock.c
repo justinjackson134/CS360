0bf273dbbdcfeb2a1728a49319f3dd64c5c56cd8
Justin Jackson <jackson.jt@att.net>
2017-03-29 20:31:03 -0700

    number of groups print still not working

--- LAB6/showblock.c
+++ LAB6/showblock.c
@@ -71,7 +71,7 @@ verifyext2fs() {
 
   //With the SuperBlock read in, you might as well print... nblocks, ninodes, ngroups, inodes_per_group, number of free inodes and blocks, etc.
   printf("\nSUPERBLOCK\n-------------------------\n - nblocks:          %d\n - ninodes:          %d\n - ngroups:          %d\n - inodes_per_group: %d\n - # free inodes:    %d\n - # free blocks:    %d\n",
-          sp->s_blocks_count, sp->s_inodes_count, (u32)sp->s_free_inodes_count/(u32)sp->s_inodes_per_group, sp->s_inodes_per_group, sp->s_free_inodes_count, sp->s_free_blocks_count);
+          sp->s_blocks_count, sp->s_inodes_count, -1, sp->s_inodes_per_group, sp->s_free_inodes_count, sp->s_free_blocks_count);
 }
 
 ///////////////////////////////////////////////////////////////
