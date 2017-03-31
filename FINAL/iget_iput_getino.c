609106b19164fbadbb5ed83e9a54bd2eba4000db
Justin Jackson <jackson.jt@att.net>
2017-03-30 17:11:29 -0700

    commenting

--- FINAL/iget_iput_getino.c
+++ FINAL/iget_iput_getino.c
@@ -84,7 +84,7 @@ init() // Initialize data structures of LEVEL-1:
   proc[0]->cwd = 0;
 
   proc[1] = malloc(sizeof(PROC));
-  proc[1]->uid = 0;
+  proc[1]->uid = 1;
   proc[1]->pid = 0;
   proc[1]->cwd = 0;
 
@@ -160,7 +160,7 @@ int iput(MINODE *mip)  // dispose of a minode[] pointed by mip
   printf("iput: dev=%d ino=%d\n", mip->dev, mip->ino); 
 
   //Use mip->ino to compute 
-  //blk containing this INODE
+  //blk containing this INODE <-----------------------------------------------------------------------Is this right?
   //disp of INODE in blk
   blk  = (mip->ino-1)/8 + iblock;  // iblock = Inodes start block #
   disp = (mip->ino-1) % 8;
