8ac0e0193ea0df9c624a2bfddd38f43c4135f931
Justin Jackson <jackson.jt@att.net>
2017-04-16 19:47:44 -0700

    Changes

--- FINAL/FS.c
+++ FINAL/FS.c
@@ -1,4 +1,13 @@
-/********* showblock.c code ***************/
+////////////////////////////////////////////////////////////////////
+// EXT2 FILE SYSTEM                                               //
+// 4/16/2017                                                      //
+// JACOB SADOIAN                                                  //
+// JUSTIN JACKSON                                                 //
+////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////
+// INCLUDES                                                       //
+////////////////////////////////////////////////////////////////////
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
@@ -6,6 +15,9 @@
 #include <string.h> // Used for strtok
 #include <sys/stat.h> // Used for S_ISDIR
 
+////////////////////////////////////////////////////////////////////
+// TYPEDEFS                                                       //
+////////////////////////////////////////////////////////////////////
 typedef unsigned char  u8;
 typedef unsigned short u16;
 typedef unsigned int   u32;
@@ -16,25 +28,32 @@ typedef struct ext2_super_block SUPER;
 typedef struct ext2_inode       INODE;
 typedef struct ext2_dir_entry_2 DIR;    // need this for new version of e2fs
 
+
+////////////////////////////////////////////////////////////////////
+// DEFINES                                                        //
+////////////////////////////////////////////////////////////////////
+#define BLKSIZE 1024
+
+
+////////////////////////////////////////////////////////////////////
+// GLOBAL VARIABLES                                               //
+////////////////////////////////////////////////////////////////////
+int isDebug = 1; // IF 1, enable debug prints!
+
 GD    *gp;
 SUPER *sp;
 INODE *ip;
 DIR   *dp; 
 
-#define BLKSIZE 1024
-
-
-
-
-
-
-
-
-
-///////////////////////////////////////////////////////////////
-// Vars for getblock()
 char buf[BLKSIZE];
 int fd;
+
+
+
+
+////////////////////////////////////////////////////////////////////
+// FUNCTIONS                                                      //
+////////////////////////////////////////////////////////////////////
 
 // Gets a block from the file descriptor
 int get_block(int fd, int blk, char buf[ ]) {
@@ -54,20 +73,7 @@ int get_block(int fd, int blk, char buf[ ]) {
 ///////////////////////////////////////////////////////////////
 // Checks to make sure that the open fs is ext2
 verifyext2fs() {
-
-  printf("!!!!!!!!!!!!!!!!!!! FILE descriptor: %d", fd);
-  printf("buf: %d", buf);
-
-  // read SUPER block
-  get_block(fd, 1, buf);  
-  sp = (SUPER *)buf;
-
-  // check for EXT2 magic number:
-  printf("VALID EXT2 FS: s_magic = %x\n", sp->s_magic);
-  if (sp->s_magic != 0xEF53) {
-    printf("NOT an EXT2 FS\n");
-    exit(1);
-  }
+  
 
   //With the SuperBlock read in, you might as well print... nblocks, ninodes, ngroups, inodes_per_group, number of free inodes and blocks, etc.
   printf("\nSUPERBLOCK\n-------------------------\n - nblocks:          %d\n - ninodes:          %d\n - inodes_per_group: %d\n - # free inodes:    %d\n - # free blocks:    %d\n",
@@ -78,6 +84,63 @@ verifyext2fs() {
 
 
 
+void mountRoot(char *name)
+{   
+  // Open disk for read/write
+  fd = open(disk, O_RDWR);
+  // Check if the open succeds or fails
+  if (fd < 0) {
+    // If we failed, exit!
+    printf("Open failed\n");
+    exit(1);
+  }
+  // If we reach here, the file opened successfully
+  printf("Opened '%s' successfully\n", disk);
+
+  // read the SUPER block into the buffer
+  get_block(fd, 1, buf);
+
+  // Set the super pointer == buf
+  sp = (SUPER *)buf;
+
+  if(isDebug)
+  {
+    printf("\nSUPERBLOCK\n-------------------------\n - nblocks:          %d\n - ninodes:          %d\n - inodes_per_group: %d\n - # free inodes:    %d\n - # free blocks:    %d\n",
+          sp->s_blocks_count, sp->s_inodes_count, sp->s_inodes_per_group, sp->s_free_inodes_count, sp->s_free_blocks_count);
+  }
+
+  // check for EXT2 magic number:
+  // If we are invalid, inform user and exit
+  if (sp->s_magic != 0xEF53) {
+    printf("INVALID EXT2 FS: s_magic = %x\n", sp->s_magic);
+    exit(1);
+  } 
+  // Else we are a valid file system!
+  else
+  {  
+    printf("VALID EXT2 FS: s_magic = %x\n", sp->s_magic);
+
+    // Read Group Descriptor block
+    get_block(dev, 2, buf);
+    
+    // Set the Group Descriptor pointer == buf
+    gp = (SUPER *)buf;
+    
+    // Set InodesBeginBlock
+    InodesBeginBlock = gp->bg_inode_table;
+    if(isDebug) printf("\nInodesBeginBlock: %d\n", InodesBeginBlock);
+
+    /*// WHAT DOES THIS DO?
+    root = iget(dev, 2);
+    root->mountptr = (MOUNT *)malloc(sizeof(MOUNT));
+    root->mountptr->ninodes = ninodes;
+    root->mountptr->nblocks = nblocks;
+    root->mountptr->dev = dev;
+    root->mountptr->busy = 1;
+    root->mountptr->mounted_inode = root;
+    */
+  }
+}
 
 
 
@@ -86,8 +149,9 @@ verifyext2fs() {
 
 
 
-
-
+////////////////////////////////////////////////////////////////////
+// MAINLINE                                                       //
+////////////////////////////////////////////////////////////////////
 
 // Vars for mainline
 // Name of disk to open
@@ -95,17 +159,12 @@ char *disk = "mydisk";
 
 // Mainline handles opening of disk, then calls showblock()
 main(int argc, char *argv[ ]) { 
-  	// If given a diskname, use it instead of mydisk - DEFAULT: "mydisk"
-  	if (argc > 1) {
-    	disk = argv[1];
-  	}
-  	// Open disk for read only
-  	fd = open(disk, O_RDONLY);
-  	if (fd < 0) {
-    	printf("Open failed\n");
-    	exit(1);
-  	}
-  	printf("Opened '%s' for RDONLY\n", disk);
+    // If given a diskname, use it instead of mydisk - DEFAULT: "mydisk"
+    if (argc > 1) {
+      disk = argv[1];
+    }
+    // MOUNT ROOT
+    mountRoot(disk);
 
-  	verifyext2fs();  
+    
 }
