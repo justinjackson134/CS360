3b7b79def2e26202839a1e897030045c8f0536d0
Justin Jackson <jackson.jt@att.net>
2017-04-20 15:06:41 -0700

    name fix?

--- FINAL/FS.c
+++ FINAL/FS.c
@@ -1084,6 +1084,8 @@ int my_make_dir_Helper(MINODE *parentMinodePtr, char *name)
 	// get the parent MINODES block into buf (CALLED IN NEXT FUNCTION)
 	//get_block(fd, parentMinodePtr->INODE.i_block[0], buf);
 
+	// We never set mips name! We may not be initializing this as much as needed!!!!
+	mip->name = child;
 	printf("Going to Enter_Name: name = %s\n", mip->name);
 	enter_name(parentMinodePtr, mip->ino, mip->name);
 }
