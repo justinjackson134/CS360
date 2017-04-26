56e1d0ca7a8901746b0af4a2cb6ea5adc235e7b7
Justin Jackson <jackson.jt@att.net>
2017-04-25 20:44:56 -0700

    format the command line all pretty

--- FINAL/FS.c
+++ FINAL/FS.c
@@ -2756,9 +2756,10 @@ main(int argc, char *argv[ ]) {
   while(1)
   {
     // Print prompt for user command entry
-    printf("\nJ&J EXT2FS@");
+    printf("UID-%d@J&J EXT2FS:~", running->uid);
+    printf("J&J EXT2FS@");
     recursive_pwd(running->cwd, 0);
-    printf(": ");
+    printf("$ ");
     // Read in and tokenize Command
     numberOfCommands = tokenizeCommmand();
     if (strcmp(command[0], "quit") == 0)
