c3fd2ae3fe3088a193e38b1bad4b9ae485024450
Justin Jackson <jackson.jt@att.net>
2017-04-25 22:37:56 -0700

    fix ls?

--- FINAL/FS.c
+++ FINAL/FS.c
@@ -670,6 +670,7 @@ void my_ls(char *name) {
 					// Print printMe's info
 					if ((printMe->INODE.i_mode & 0120000) == 0120000)
 					{
+						strcpy(command[1],dir->name);
 						read_link(dir->name);
 					}
 					else
@@ -723,6 +724,7 @@ void my_ls(char *name) {
 					// Print printMe's info
 					if ((printMe->INODE.i_mode & 0120000) == 0120000)
 					{
+						strcpy(command[1],dir->name);
 						read_link(dir->name);
 					}
 					else
