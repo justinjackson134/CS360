a4b4f163266b92146c3820098b4d303e2b974b94
Justin Jackson <jackson.jt@att.net>
2017-04-25 20:17:46 -0700

    print octal

--- FINAL/FS.c
+++ FINAL/FS.c
@@ -668,7 +668,7 @@ void my_ls(char *name) {
 					// Load the parents inode into a MINODE
 					printMe = iget(fd, ino);
 					// Print printMe's info
-					printf("%s\t%u\t%d\t%d\t%d\t%d\n", dir->name, printMe->INODE.i_mode, printMe->INODE.i_ctime, printMe->INODE.i_mtime, printMe->INODE.i_atime, printMe->INODE.i_links_count);					
+					printf("%s\t%o\t%d\t%d\t%d\t%d\n", dir->name, printMe->INODE.i_mode, printMe->INODE.i_ctime, printMe->INODE.i_mtime, printMe->INODE.i_atime, printMe->INODE.i_links_count);					
 
 					// Put the MINODE back into fd
 					iput(printMe);
@@ -714,7 +714,7 @@ void my_ls(char *name) {
 					// Load the parents inode into a MINODE
 					printMe = iget(fd, ino);
 					// Print printMe's info
-					printf("%s\t%u\t%d\t%d\t%d\t%d\n", dir->name, printMe->INODE.i_mode, printMe->INODE.i_ctime, printMe->INODE.i_mtime, printMe->INODE.i_atime, printMe->INODE.i_links_count);					
+					printf("%s\t%o\t%d\t%d\t%d\t%d\n", dir->name, printMe->INODE.i_mode, printMe->INODE.i_ctime, printMe->INODE.i_mtime, printMe->INODE.i_atime, printMe->INODE.i_links_count);					
 
 
 					cp += dir->rec_len;
