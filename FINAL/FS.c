beeff837ae0bdcefc67edc35826b3f4656a5fa9f
Justin Jackson <jackson.jt@att.net>
2017-04-26 21:07:29 -0700

    symlink fixing

--- FINAL/FS.c
+++ FINAL/FS.c
@@ -2451,7 +2451,7 @@ void sym_link(char *oldPath, char *newPath)
 		// Get the In_MEMORY minode of parent:
 		if (isDebug) printf("Setting parentMinodePtr\n");
 		Omip = iget(root->dev, parentInode);
-		/*
+		
     childInode = search(Omip, child);
 
 		// Check if child directory does not exist
@@ -2464,7 +2464,7 @@ void sym_link(char *oldPath, char *newPath)
 		// set child Minodeptr
 		if (isDebug) printf("Setting childMinodePtr\n");
 		Omip = iget(root->dev, childInode);
-    */
+    
 	}
 
 	// If the child exists, then we can continue, else, exit
